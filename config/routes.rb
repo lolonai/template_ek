957d31e9950d0b078eb6edc6e4f3ee6cef1689c8
laurent naitza <lolonaitza@gmail.com>
2020-03-18 12:27:43 +0100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/new'
+  get 'articles/create'
+  get 'articles/update'
+  get 'articles/edit'
+  get 'articles/delete'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
